5404fe0593d317f68259639700b2d7fb02143482
Mauro <mauro@osn.global>
2020-02-21 13:41:37 -0300

    new turn

--- src/constants/iceServers.js
+++ src/constants/iceServers.js
@@ -1,4 +1,9 @@
 export default [
+    {urls: "stun:voip0.prosky.co:3478"},
+    {urls: "turn:voip0.prosky.co:3478?transport=udp",
+      username: "voip",
+      credential: "fUnWS9QP"
+    },
     { urls: "stun:stun.l.google.com:19302" }, 
     { urls: "stun:stun1.l.google.com:19302" }, 
 ];
