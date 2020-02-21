415fb09b1132225b03e8b6fd35db84447d76d92b
Mauro <mauro@osn.global>
2020-02-21 13:34:06 -0300

    replace stun urls

--- src/constants/iceServers.js
+++ src/constants/iceServers.js
@@ -1,4 +1,4 @@
 export default [
-    { url: "stun:stun.l.google.com:19302" }, 
-    { url: "stun:stun1.l.google.com:19302" }, 
+    { urls: "stun:stun.l.google.com:19302" }, 
+    { urls: "stun:stun1.l.google.com:19302" }, 
 ];
