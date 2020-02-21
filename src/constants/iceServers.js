ed31dfd810e09fa1bf82b1f92d8655b94f1edae6
Mauro <mauro@osn.global>
2020-02-20 22:31:40 -0300

    ice rework

--- src/constants/iceServers.js
+++ src/constants/iceServers.js
@@ -1,10 +1,4 @@
 export default [
     { url: "stun:stun.l.google.com:19302" }, 
-    { url: "stun:stun2.l.google.com:19302" }, 
-    { url: "stun.ekiga.net" },
-    { url: "stun.ideasip.com" },
-    { url: "stun.rixtelecom.se" },
-    { url: "stun.schlund.de" },
-    { url: "stun3.l.google.com:19302" },
-    { url: "stun4.l.google.com:19302" }
+    { url: "stun:stun1.l.google.com:19302" }, 
 ];
