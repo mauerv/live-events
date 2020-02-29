2cc812732f1765b99970395afc54e36c49b4e1e4
Mauro <mauro@osn.global>
2020-02-29 15:12:17 -0300

    setup mine and gugl stun server, temporarily remote turn

--- src/constants/iceServers.js
+++ src/constants/iceServers.js
@@ -1,8 +1,5 @@
 export default [
     { urls: "stun:54.244.72.119:3478" },
-    { urls: "turn:54.244.72.119:3478",
-        username: "voip",
-        credential: "m0fn9db8s"
-    },
     { urls: "stun:stun.l.google.com:19302" }, 
 ];
+
