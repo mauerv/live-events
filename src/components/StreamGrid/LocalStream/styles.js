09e6fc609ea73d466af225246ba222a63a841cf9
Mauro <mauro@osn.global>
2020-03-05 18:02:17 -0300

    adjust UI for 4 people

--- src/components/StreamGrid/LocalStream/styles.js
+++ src/components/StreamGrid/LocalStream/styles.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import Grid from "@material-ui/core/Grid";
 
 export const StreamContainer = styled(({ children, ...rest }) => (
-  <Grid item md={4} xs={6} {...rest}>
+  <Grid item xs={6} {...rest}>
     {children}
   </Grid>
 ))``;
